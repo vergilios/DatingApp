14b2f1e51cae720e3a40b62881adef9439384f10
SOFTCOM-INT\lisp <s.linardakis@im-systems.de>
2019-11-19 15:21:29 +0200

    part 6

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,29 +8,31 @@ import { AuthService } from '../_services/auth.service';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-
   model: any = {};
 
-  constructor(private authService: AuthService ) { }
+  constructor(
+    public authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   login() {
-    this.authService.login(this.model).subscribe(next => {
-      console.log("Success");
-    },
-    error => {
-      console.log(error);
-    });
+    this.authService.login(this.model).subscribe(
+      next => {
+        this.alertify.success('logged in');
+      },
+      error => {
+        this.alertify.error(error);
+      }
+    );
   }
 
   loggedIn() {
-    const token = localStorage.getItem("token");
-    return !!token;
+    return this.authService.loggedIn();
   }
   logout() {
-    localStorage.removeItem("token");
-    console.log("Logged out");
+    localStorage.removeItem('token');
+    this.alertify.message('Logged out');
   }
 }
